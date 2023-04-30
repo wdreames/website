bdcf0951771e1786c06b1797f9397a6f55fcc7d9
William Reames <william.reames.19@cnu.edu>
2023-04-30 00:53:35 +0000

    Created a working contact me form

--- contact_form.php
+++ contact_form.php
@@ -1,20 +1,48 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+function sendMail($name, $replyto, $message) {
+    $receiver_email = "wdreames@gmail.com";
+    $server_email = "william.reames.website@gmail.com";
+    $server_name = "william-reames.com";
+    $subject = "New message on my website from $name";	
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
+
+    $mail->SMTPDebug  = 0;  
+    $mail->SMTPAuth   = TRUE;
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+    $mail->Host       = "smtp.gmail.com";
+    $mail->Username   = "$_SERVER[GMAIL_USER]";
+    $mail->Password   = "$_SERVER[GMAIL_PASS]";
+    
+    $mail->IsHTML(true);
+    $mail->AddAddress("$receiver_email");
+    $mail->SetFrom("$server_email", "$server_name");
+    $mail->AddReplyTo("$replyto", "$name");
+    $mail->Subject = "$subject";
+
+    $mail->MsgHTML($message);
+    return $mail->Send();
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = trim($_POST['name']);
     $email = trim($_POST['email']);
     $message = trim($_POST['message']);
 
-    // Validate form data
     if (empty($name) || empty($email) || empty($message)) {
-        echo 'Please fill in all fields';
+        echo 'Please fill in all fields.';
     } else {
-        $to = '<william.reames.19@cnu.edu>';
-        $subject = 'New message from my website!';
-        $message = wordwrap($message,70);
-        $headers = "From: <$email>";
-
-        $mail_result = mail($to, $subject, $message, $headers);
-
+        $mail_result = sendMail($name, $email, $message);
         if($mail_result){
             echo 'Message sent successfully.'; 
         }
@@ -23,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 } else {
-    // If the request method is not POST, redirect to the contace page
+    // If the request method is not POST, redirect to the contact page
     header('Location: /contact');
     exit;
 }
